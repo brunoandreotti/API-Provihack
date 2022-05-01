37cf1fcfaa31dc08d25d9f3b2699ca85468e837d
Bruno Andreotti <brunoandreotti@outlook.com>
2022-05-01 15:04:44 -0300

    :recycle:  exclude donor password at product info

--- src/services/product/findAllProducts.ts
+++ src/services/product/findAllProducts.ts
@@ -4,7 +4,7 @@ import { Product } from "../../models/Product.model";
 export class FindAllProductsService {
   static async execute() {
     try {
-      const newProduct = Product.findAll({include: [Donor]}, )
+      const newProduct = Product.findAll({include: [{model: Donor, attributes: {exclude: ["password"]}}]})
       
       return newProduct
     } catch (error) {
